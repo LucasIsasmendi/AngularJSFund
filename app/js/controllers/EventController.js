e4c303ca4eca3c8b29b6996cdf3c0f7832ca8a9e
LucasIsasmendi <tartalario@yahoo.com.ar>
2014-09-18 19:41:30 -0300

    promises with : doesn't work - fix it

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -3,9 +3,19 @@
 eventsApp.controller('EventController', 
 	function EventController($scope, eventData){
 		$scope.sortorder = 'name';
+		$scope.event = eventData.getEvent();
+		$scope.event.then(
+			function(event) {
+				console.log(event);
+			}, function(status) {
+				console.log(status);
+			}
+		);
+
+		/*
 		eventData.getEvent(function (event){
 			$scope.event = event;
-		});
+		});*/
 		$scope.upVoteSession = function(session){
 			session.upVoteCount++;
 		};
--- app/js/services/EventData.js
+++ app/js/services/EventData.js
@@ -1,14 +1,20 @@
-eventsApp.factory('eventData', function ($http, $log) {
+eventsApp.factory('eventData', function ($http, $q) {
 	return {
-		getEvent: function (successcb) {
+		getEvent: function () {
+			var deferred = $q.defer();
+
 			$http({method: 'GET', url: '/app/data/event/1.json'}).
 				success(function (data, status, headers, config) {
-					$log.info(data, status, headers(), config);
-					successcb(data);
+					//$log.info(data, status, headers(), config);
+					//successcb(data);
+					deferred.resolve(data);
 				}).
 				error(function (data, status, headers, config) {
-					$log.warn(data, status, headers, config);
+					//$log.warn(data, status, headers, config);
+					deferred.reject(status);
 				});
+				
+			return deferred.promise;
 		}
 	};
 });
