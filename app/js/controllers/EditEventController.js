3fdbb96b6ce728074bda9b32fa0e1c1083bfbddf
LucasIsasmendi <tartalario@yahoo.com.ar>
2014-09-18 22:56:11 -0300

    Add new event and save it in /data/event

--- app/js/controllers/EditEventController.js
+++ app/js/controllers/EditEventController.js
@@ -1,15 +1,24 @@
 'use strict';
 
 eventsApp.controller('EditEventController',
-	function EditEventController($scope){
-		$scope.saveEvent = function(event, newEventForm){
-			console.log(newEventForm);
-			if(newEventForm.$valid){
+	function EditEventController($scope, eventData){
+		
+		$scope.event = {};
+
+		$scope.saveEvent = function(event, form){
+			//console.log(newEventForm);
+			if(form.$valid){
 				window.alert('event ' + event.name + ' saved!!');
+				eventData.save(event)
+					.then(
+						function(response) { console.log('success', response)},
+						function(response) { console.log('failure', response)}
+					);
 			}
-		}
+		};
+		
 		$scope.cancelEdit = function(){
 			window.location = "EventDetails.html";
-		}
+		};
 	}
 );
--- app/js/services/EventData.js
+++ app/js/services/EventData.js
@@ -1,14 +1,23 @@
 eventsApp.factory('eventData', function ($resource, $q) {
+	var resource = $resource('/app/data/event/:id', {id:'@id'});
 	return {
 		getEvent: function () {
 			var deferred = $q.defer();
-			$resource('/app/data/event/:id', {id:'@id'})
-				.get({id:1}, function (event) {
+				resource.get({id:1}, function (event) {
 					deferred.resolve(event);
 				},
 				function (response) {
 					deferred.reject(response);
 				});
+			return deferred.promise;
+		},
+		save: function(event) {
+			var deferred = $q.defer();
+			event.id = 999;
+			resource.save(event, 
+				function(response) { deferred.resolve(response);},
+				function(response) { deferred.reject(response);}
+			);
 			return deferred.promise;
 		}
 	};
