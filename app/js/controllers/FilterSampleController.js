21a6f4cf76c0232a5076bbd78cbfdf5b98830d48
LucasIsasmendi <tartalario@yahoo.com.ar>
2014-09-19 19:19:03 -0300

    3.14b using <name>Filter ton inject a filter

--- app/js/controllers/FilterSampleController.js
+++ app/js/controllers/FilterSampleController.js
@@ -1,13 +1,12 @@
 'use strict';
 
 eventsApp.controller('FilterSampleController',
-	function FilterSampleController($scope, $filter) {
+	function FilterSampleController($scope, durationsFilter) {
 		$scope.data = {};
 
-		var durations = $filter('durations');
-		$scope.data.duration1 = durations(1);
-		$scope.data.duration2 = durations(2);
-		$scope.data.duration3 = durations(3);
-		$scope.data.duration4 = durations(4);
+		$scope.data.duration1 = durationsFilter(1);
+		$scope.data.duration2 = durationsFilter(2);
+		$scope.data.duration3 = durationsFilter(3);
+		$scope.data.duration4 = durationsFilter(4);
 	}
 );
