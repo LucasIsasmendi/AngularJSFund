fb8438cb466782492b9b4157fd1a879dd53af7aa
LucasIsasmendi <tartalario@yahoo.com.ar>
2014-09-20 04:23:46 -0300

    4.05 add events, event details

--- app/js/app.js
+++ app/js/app.js
@@ -12,5 +12,10 @@ var eventsApp = angular.module('eventsApp', ['ngResource'])
 				templateUrl: 'templates/EventList.html',
 				controller: 'EventListController'
 			});
+		$routeProvider.when('/event/:eventId',
+			{
+				templateUrl: 'templates/EventDetails.html',
+				controller: 'EventController'
+			});		
 	});
 
